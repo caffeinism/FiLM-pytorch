e671afec215bfe2660706eb0234e2b5538d17066
Minjong Kim <39237502+MakeDirtyCode@users.noreply.github.com>
2019-12-04 13:13:08 +0900

    Update tf_recorder.py

--- tf_recorder.py
+++ tf_recorder.py
@@ -1,12 +1,13 @@
 from tensorboardX import SummaryWriter
 import os, sys
+import os.path
 
 # https://github.com/nashory/pggan-pytorch/blob/master/tf_recorder.py
 class tf_recorder:
-    def __init__(self, network_name):
-        os.system('mkdir -p repo/tensorboard')
+    def __init__(self, network_name, logdir='repo/tensorboard'):
+        os.system('mkdir -p {}'.format(logdir))
         for i in range(1000):
-            self.targ = 'repo/tensorboard/{}_{}'.format(network_name, i)
+            self.targ = os.path.join(logdir, '{}_{}'.format(network_name, i))
             if not os.path.exists(self.targ):
                 self.writer = SummaryWriter(self.targ)
                 break
